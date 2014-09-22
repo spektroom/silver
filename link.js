4e3849c58d79dc4bdf4beb03f42c6ece3ef3e3f4
spektroom <spektroom@yandex.ru>
2014-09-22 11:41:16 +0400

    Create link.js

---
+++ link.js
@@ -0,0 +1,10 @@
+class Squarel {
+public static void main (String args[])
+{
+int a, b = 4, s; 
+a = 5; 
+s = a * b ;
+System.out.println ("Площадь прямоугольника со сторонами 5 см 
+и 4 см равна " + s + " квадратных сантиметров.");
+} 
+}
